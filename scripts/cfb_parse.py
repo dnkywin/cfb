0697c8dedb5ffb3a785436386f55c2264fb57e08
dnkywin <calvindeng@college.harvard.edu>
2013-11-24 22:53:25 -0500

    Add in home team capabilities

--- scripts/cfb_parse.py
+++ scripts/cfb_parse.py
@@ -18,9 +18,26 @@ for line in data.split('\n')[1:]:
             games[gameid] = []
         games[gameid]+=[teamid,pts]
 
+WEEKNUM = int(sys.argv[1])
+inp = open("data/game.csv","r")
+data = inp.read()
+inp.close()
+
+for line in data.split('\n')[1:]:
+    if line:
+        datum = line.split(',')
+        gameid = datum[0]
+        if gameid in games:
+            res = games[gameid]
+            hometeam = int(datum[3])
+            assert(hometeam==res[0] or hometeam == res[2])
+            if datum[-1] == 'TEAM': games[gameid].append(hometeam)
+            else: games[gameid].append(-1)
+        
+
 outp = open("data/games-parsed-week%s.txt"%(WEEKNUM,),"w")
 
 for game in games:
     res = games[game]
-    outp.write("%s %s %s %s %s\n" % ((game,)+tuple(res)))
+    outp.write("%s %s %s %s %s %s\n" % ((game,)+tuple(res)))
 outp.close()
