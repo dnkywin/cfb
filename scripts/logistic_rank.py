a6629f1bed9fefdfe0b80fdc6a8eebd5bf921fc4
dnkywin <calvindeng@college.harvard.edu>
2013-11-27 18:42:12 -0500

    Got rid of unnecessary stuff in logistic rank

--- scripts/logistic_rank.py
+++ scripts/logistic_rank.py
@@ -35,30 +35,8 @@ while not stable:
 
 ranks = sorted([(power[i],i) for i in wins if len(wins[i])+len(loss[i])>4])
 
-outp = open("ranks/logistic/%s_rank_week%s.txt"%(METHOD, WEEKNUM),"w")
+outp = open("ranks/%s/%s_rank_week%s.txt"%(METHOD,METHOD, WEEKNUM),"w")
 for i,j in enumerate(reversed(ranks)):
     outp.write("%s. %s (%s-%s)\n" % (i+1, names[j[1]],len(wins[j[1]]), len(loss[j[1]])))#, int(1200+400*math.log(j[0]/MU)/math.log(2))))
 
 outp.close()
-
-scores = {}
-for g in games:
-    game = games[g]
-    tup1 = (game[1],game[3])
-    tup2 = (game[3],game[1])
-    if tup1 not in scores:
-        scores[tup1] = []
-        scores[tup2] = []
-    if (power[game[0]]>power[game[2]]):
-        scores[tup1].append(1.0)
-        scores[tup2].append(0.0)
-    elif power[game[2]]>power[game[0]]:
-        scores[tup1].append(0.0)
-        scores[tup2].append(1.0)
-
-l = [score for score in scores if score[0]>score[1]]
-freq = sorted(l, key = lambda x: -len(scores[x]))
-highprob = sorted(l, key = lambda x: -sum(scores[x])/len(scores[x]))
-
-#for i in highprob:
-#    print "%s wins: %s losses: %s" % (i, sum(scores[i]), len(scores[i])-sum(scores[i]))
