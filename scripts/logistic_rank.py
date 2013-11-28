9a4ca5b2d6f74fcf37f1d5a42901e88d0d4fb379
dnkywin <calvindeng@college.harvard.edu>
2013-11-27 20:56:57 -0500

    A little more trimming

--- scripts/logistic_rank.py
+++ scripts/logistic_rank.py
@@ -37,6 +37,6 @@ ranks = sorted([(power[i],i) for i in wins if len(wins[i])+len(loss[i])>4])
 
 outp = open("ranks/%s/%s_rank_week%s.txt"%(METHOD,METHOD, WEEKNUM),"w")
 for i,j in enumerate(reversed(ranks)):
-    outp.write("%s. %s (%s-%s)\n" % (i+1, names[j[1]],len(wins[j[1]]), len(loss[j[1]])))#, int(1200+400*math.log(j[0]/MU)/math.log(2))))
+    outp.write("%s. %s (%s-%s)\n" % (i+1, names[j[1]],len(wins[j[1]]), len(loss[j[1]])))
 
 outp.close()
